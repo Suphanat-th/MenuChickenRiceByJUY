e7654b0442508e3d4d656b73109ec6c0bd74135e
Suphanat Boss <70587200+Suphanat-th@users.noreply.github.com>
2024-03-18 22:06:25 +0700

    Update next.config.mjs

--- menujuy/next.config.mjs
+++ menujuy/next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    exportPathMap: async function (defaultPathMap) {
-        return {
-          '/': { page: '/' },
-          // Add other routes if needed
-        };
-    },
     reactStrictMode: true,
 };
 
