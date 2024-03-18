3ca035872087122537278cb47457c04a4c18f756
Suphanat Boss <70587200+Suphanat-th@users.noreply.github.com>
2024-03-18 23:49:54 +0700

    Update next.config.mjs

--- menujuy/next.config.mjs
+++ menujuy/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    output:"export",
     basePath: process.env.NODE_ENV === 'production' ? '/your-username.github.io/<your-repo-name>' : '', // Replace with your details
     distDir: 'out', // Update this path as per your requirement
 };
