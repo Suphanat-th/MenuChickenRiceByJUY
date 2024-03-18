4db02b11ae67ef757b03ef7ac443be0c05e6a8e2
Suphanat Boss <70587200+Suphanat-th@users.noreply.github.com>
2024-03-18 22:21:16 +0700

    Update next.config.mjs
    set output to /menujuy

--- menujuy/next.config.mjs
+++ menujuy/next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
     output: 'export',
     // reactStrictMode: true,
+    output: {
+        // This allows you to export to static HTML files
+        // Optionally, you can specify a subdirectory here
+        // For example, if you want to export to '/out' directory:
+        // path: '/out'
+        // By default, it exports to the root directory
+        path: '/menujuy', // Update this path as per your requirement
+      },
 };
 
 export default nextConfig;
