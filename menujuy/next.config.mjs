ea0eeef4ddb6467ea4d552616cd25ea66e69fae7
Suphanat Boss <70587200+Suphanat-th@users.noreply.github.com>
2024-03-18 22:04:06 +0700

    Update next.config.mjs
    set route

--- menujuy/next.config.mjs
+++ menujuy/next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    exportPathMap: async function (defaultPathMap) {
+        return {
+          '/': { page: '/' },
+          // Add other routes if needed
+        };
+    },
     reactStrictMode: true,
 };
 
