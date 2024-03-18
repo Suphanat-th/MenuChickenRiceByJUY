12f8e905ade41e4785a5ebb8ac9102f8bbae3534
Suphanat Boss <70587200+Suphanat-th@users.noreply.github.com>
2024-03-18 23:52:17 +0700

    Update next.config.mjs

--- menujuy/next.config.mjs
+++ menujuy/next.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    output:"export",
+    reactStrictMode: true, 
+    output: {
+        // Specify the directory where the exported files should be generated
+        // Optionally, you can specify a subdirectory here
+        // For example, if you want to export to '/out' directory:
+        // dir: '/out'
+        // By default, it exports to the root directory
+        dir: 'out', // Update this path as per your requirement
+      },
     basePath: process.env.NODE_ENV === 'production' ? '/your-username.github.io/<your-repo-name>' : '', // Replace with your details
-    distDir: 'out', // Update this path as per your requirement
 };
 
 export default nextConfig;
