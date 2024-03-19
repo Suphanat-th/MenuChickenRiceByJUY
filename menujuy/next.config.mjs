d7445eed41eb046c1fff5bc504fc4d5554efcc20
Suphanat Boss <70587200+Suphanat-th@users.noreply.github.com>
2024-03-19 09:16:32 +0700

    Update next.config.mjs

--- menujuy/next.config.mjs
+++ menujuy/next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath:"/MenuChickenRiceByJUY",
-    output: {
+    output: "export",
+    //output: {
         // Specify the directory where the exported files should be generated
         // Optionally, you can specify a subdirectory here
         // For example, if you want to export to '/out' directory:
         // dir: '/out'
         // By default, it exports to the root directory
-        dir: '/out', // Update this path as per your requirement
-      },
+       // dir: '/out', // Update this path as per your requirement
+     // },
 };
 
 export default nextConfig;
